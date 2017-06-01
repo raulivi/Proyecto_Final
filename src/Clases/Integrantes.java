15c95115a019264263a26e52f8b263a74cb3b98b
Raul <Raul@Raul-PC>
2017-05-31 21:36:38 -0600

    La clase Integrantes llevara solo nuestros nombres y carnets

--- src/Clases/Integrantes.java
+++ src/Clases/Integrantes.java
@@ -3,5 +3,14 @@ package Clases;
 
 
 public class Integrantes {
-    
+    String Integrante1="";
+    String Carnet1="";
+    String Integrante2="";
+    String Carnet2="";
+    String Integrante3="";
+    String Carnet3="";
+    String Integrante4="";
+    String Carnet4="";
+    String Integrante5="Jose Raul Escalante Aparicio";
+    String Carnet5="389216";
 }
